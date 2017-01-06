79a7c6164ed39bdeccb962f771f849cc72714d53
vhalbert <vhalbert@jboss.org>
2017-01-06 14:14:33 -0500

    initial commit

---
+++ en-US/files/pressgang_website.js
@@ -0,0 +1,8 @@
+pressgang_website_callback([
+	{"topicId":43235,"target":"appe-Revision_History","title":"Revision History","newSince":""},
+	{"topicId":13540,"target":"Download_API_Documentation","title":"Download API Documentation","newSince":""},
+	{"topicId":13413,"target":"Variable_Name_EAP_HOME","title":"Variable Name: EAP_HOME","newSince":""},
+	{"topicId":7077,"target":"Red_Hat_Documentation_Site","title":"Red Hat Documentation Site","newSince":""},
+	{"topicId":6842,"target":"Back_Up_Your_Data","title":"Back Up Your Data","newSince":""},
+	{"topicId":13421,"target":"Variable_Name_PROFILE","title":"Variable Name: MODE","newSince":""},
+	{"topicId":24063,"target":"Author_Group","title":"Author Group","newSince":""}], 23066, "Red Hat JBoss Data Virtualization", "API Documentation", "6.2");
